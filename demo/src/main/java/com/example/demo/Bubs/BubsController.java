cd8931b2f5a5f4b4c190123ff84de217f3279f97
Manuel Hamonangan <manhsa4002@gmail.com>
2024-11-25 22:46:18 +0700

    konek db pake spring, fitur login admin

---
+++ demo/src/main/java/com/example/demo/Bubs/BubsController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Bubs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+// @RequestMapping("/login/bubs/logout")
+public class BubsController {
+    
+    // @GetMapping
+    // public String goBack() {
+    //     return "login";
+    // }
+}
